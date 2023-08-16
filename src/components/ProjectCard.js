0ecd3b8a05584e105c2f80004f5d7c5da2f927a7
Polina Fomicheva <puffyoluv@yandex.ru>
2023-08-17 01:40:15 +0300

    projects links

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,11 +4,11 @@ export const ProjectCard = ({ links, title, description, imgUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-      <a href={links}>
         <img src={imgUrl} />
-        </a>
         <div className="proj-txtx">
+        <a className="project-title" href={links}>
           <h4>{title}</h4>
+          </a>
           <span>{description}</span>
         </div>
       </div>
